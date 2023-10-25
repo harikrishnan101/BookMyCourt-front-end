65661ee0e91bcb6ef2edae1f17e2c1b387b6d15e
HARIKRISHNAN R <harikrishnan183hari@gmail.com>
2023-10-25 16:48:59 +0530

    modal update

--- src/components/CourtTimeTable.jsx
+++ src/components/CourtTimeTable.jsx
@@ -2,8 +2,64 @@ import Table from 'react-bootstrap/Table';
 import Courtmodal from "../components/common/Modal";
 import { useState } from 'react';
 
-function CourtTimeTable({data}) {
+import './style/CourtTimeTable.css';
+import { TIMINGS } from '../constants/baseUrl';
+import AxiosInstance from '../config/axiosinstance';
+
+function CourtTimeTable({ data }) {
   const [openModal, setOpenModal] = useState();
+
+  const[filterTimings,setfilterTimings]=useState(TIMINGS)
+
+  const [showDropDown, setshowDropDown] = useState(false);
+  // const [minData,setminData]=useState(new Date())
+
+  const[selectedTimings,setselectedTimings]=useState([])
+
+  const [courtTiming,setcourtTiming]=useState({
+    startDate:'',
+    endDate:'',
+    cost:''
+  })
+
+  const addNewTime=(element,index)=>{
+
+  const check =  selectedTimings.filter((timeObj)=> 
+      timeObj.id===element.id
+    ) 
+
+
+if(check.length > 0){
+  return
+}else{
+  setselectedTimings([...selectedTimings,element])
+}
+
+const filteredData =filterTimings.filter((timeObj)=>timeObj.id!==element.id)
+setfilterTimings(filteredData)
+
+  }
+  // const setStartingDate=(e)=>{
+
+  // }
+
+  const addCourtTiming = () => {
+    AxiosInstance({
+      method: 'post',
+      url: '/users/addCourtTimings',
+      data: {
+        date: courtTiming,
+        schedules: selectedTimings,
+      }
+    }).then((res) => {
+      // debugger;
+      // Handle the response here
+    });
+    
+    console.log(courtTiming, selectedTimings);
+  };
+  
+  
 
   return (
     <div>
@@ -11,10 +67,10 @@ function CourtTimeTable({data}) {
         <div className='ms-3'>
           <input type='text' />
           <input type='text' />
-          <button className="btn btn-dark">fetch data</button>
+          <button className="btn btn-primary">Fetch Data</button>
         </div>
-        <button className='me-3 btn btn-dark border rounded-3' onClick={() => setOpenModal(true)}>
-          create bookings
+        <button className='me-3 btn btn-primary border rounded-3' onClick={() => setOpenModal(true)}>
+          Create Bookings
         </button>
       </div>
       <Table responsive="md" className='mt-5'>
@@ -60,9 +116,39 @@ function CourtTimeTable({data}) {
         </tbody>
       </Table>
       <Courtmodal openModal={openModal} setOpenModal={setOpenModal}>
+        <div className='d-flex flex-column add-court-timing-modal'>
+          <div >{data?.name} </div>
+          <div>{data?.location} </div>
+          <div className='mt-4'>
+            <label htmlFor=''>Starting date</label>
+            <input className='' type='date' onChange={(e)=>setcourtTiming({...courtTiming,startDate:new Date(e.target.value)})} />
+            <label htmlFor=''>ending date</label>
+            <input className='' type='date' onChange={(e)=>setcourtTiming({...courtTiming,endDate:new Date(e.target.value)})} />
+            <label htmlFor=''> cost</label>
+            <input className='' type='number' onChange={(e)=>setcourtTiming({...courtTiming,cost:parseInt(e.target.value)})} />
+          </div>
+          <div className='cus-dropdown mt-4' onClick={() => setshowDropDown(true)}>
+            Select Timing
+            {showDropDown && (
+              <div className='cus-options' onMouseLeave={() => setshowDropDown(false)}>
+                <ul>
+                  {(filterTimings).map((element,index) => 
+                    <li onClick={()=>addNewTime(element,index)} key={element.id}>{element.name}</li>
+                  )}
+                </ul>
+              </div>
+            )}
+          </div>
+        </div>
+        {
+          selectedTimings.map((element)=>  
+          
+          <span className='border border-1 mx-2' key={element.id}>{element.name} </span>
+          )
+        }
+
         <div>
-          <div>{data?.name} name</div>
-          <div>{data?.location} location</div>
+        <button type="button" class="btn btn-primary" onClick={addCourtTiming()}>Primary</button>
         </div>
       </Courtmodal>
     </div>
--- src/components/SingleCourtComponent.jsx
+++ src/components/SingleCourtComponent.jsx
@@ -8,16 +8,15 @@ import { BASEURL } from '../constants/baseUrl';
 import './style/SingleCourt.css'
 // import { MDBCard, MDBCardImage, MDBCardOverlay, MDBCardText, MDBCardTitle } from 'mdb-react-ui-kit';
 
-function SingleCourtComponent(setcourtData) {
+function SingleCourtComponent({ setcourtData }) {
   const [singleCourt, setSingleCourt] = useState({});
   const { id } = useParams();
 
   useEffect(() => {
-    AxiosInstance.get('/users/getSingleCourtData', { params: { courtId: id } })
-      .then((res) => {
-        setSingleCourt(res.data.data);
-        setcourtData(res.data.data)
-      })
+    AxiosInstance.get('/users/getSingleCourtData', { params: { courtId: id } }).then((res) => {
+      setSingleCourt(res.data.data);
+      setcourtData(res.data.data)
+    })
       .catch((err) => {
         console.log(err);
       });
@@ -30,9 +29,10 @@ function SingleCourtComponent(setcourtData) {
 
       <div className='SingleCourt'>
         <img className='SingleCourtimage' src={`${BASEURL}/venderCourts/${singleCourt.image}`} alt='' />
-<h3 className='court-name'>{singleCourt?.name}</h3>
+        <h3 className='court-name'>{singleCourt?.name}</h3>
 
-        </div>
+
+      </div>
     </>
   );
 }
